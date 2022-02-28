4af961a1a16bb6c35bbec5e75f26d955d5bb1a6b
Andy Vo <andyvo@dhcp-wifi-8021x-155-41-51-57.bu.edu>
2022-02-27 21:49:03 -0500

    changing to merge to main

--- src/screens/testaudio.js
+++ src/screens/testaudio.js
@@ -1,55 +1,99 @@
-import * as React from 'react';
-import { Text, View, Stylesheet, Button } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Audio } from 'expo-av';
 
-/**
- * from the expo documentation: https://docs.expo.dev/versions/latest/sdk/audio/#audiorecording
- */
-export default function AudioRecord() {
-    const [recording, setRecording] = React.useState();
+export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [recordings, setRecordings] = React.useState([]);
+  const [message, setMessage] = React.useState("");
 
-    /**
-     * starting up audio recording, requesting permissions, etc.
-     * hello my name is melani and I am andy's girlfriend. i just read through this code and it looks great! i love the for loops. i definitely think this can move forward to prod - MZ
-     */
-    async function startRecording() {
-        try {
-            console.log('Requesting permissions!');
-            await Audio.requestPermissionsAsync();
-            await Audio.setAudioModeAsync({
-                allowsRecordingIOS: true,
-                playsInSilentModeIOS: true,
-            });
+  async function startRecording() {
+    try {
+      const permission = await Audio.requestPermissionsAsync();
 
-            console.log('Starting recording!');
-            const { recording } = await Audio.Recording.createAsync(
-                Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
-            );
-            setRecording(recording);
-            console.log('Recording started!');
-        } catch (err) {
-            console.error('Failed to start recording', err);
-        }
+      if (permission.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true
+        });
+        
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+
+        setRecording(recording);
+      } else {
+        setMessage("Please grant permission to app to access microphone");
+      }
+    } catch (err) {
+      console.error('Failed to start recording', err);
     }
+  }
 
-    /**
-     * stop recording and save to local uri
-     */
-    async function stopRecording() {
-        console.log('Stopping recording!');
-        setRecording(undefined);
-        await recording.stopAndUnloadAsync();
-        const uri = recording.getURI();
-        console.log('Recording stopped and stored at', uri);
-    }
+  async function stopRecording() {
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
 
-    return (
-        <View>
-            <Button
-                title={recording ? 'Stop Recording' : 'Start Recording'}
-                onPress={recording ? stopRecording : startRecording}
-            />
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI()
+    });
+
+    setRecordings(updatedRecordings);
+  }
+
+  function getDurationFormatted(millis) {
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay}:${secondsDisplay}`;
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index} style={styles.row}>
+          <Text style={styles.fill}>Recording {index + 1} - {recordingLine.duration}</Text>
+          <Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
         </View>
-    );
+      );
+    });
+  }
 
+  return (
+    <View style={styles.container}>
+      <Text>{message}</Text>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording} />
+      {getRecordingLines()}
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fill: {
+    flex: 1,
+    margin: 16
+  },
+  button: {
+    margin: 16
+  }
+});
